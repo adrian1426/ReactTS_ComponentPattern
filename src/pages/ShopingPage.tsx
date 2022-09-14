94bcd12fb62ade22f7d9830836a76ef1a7d4b31a
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-09-14 08:55:45 -0500

    state initializer y render props pattern - base para replicar estos patrones

--- src/pages/ShopingPage.tsx
+++ src/pages/ShopingPage.tsx
@@ -1,11 +1,5 @@
-import { useState } from 'react';
 import ProductCard from '../components/product/ProductCard';
 import { Product } from '../components/product/interfaces/ProductInterface';
-import Cart from '../components/common/cart/Cart';
-
-interface IProductInCart extends Product {
-  count: number;
-}
 
 const products: Product[] = [
   {
@@ -25,62 +19,15 @@ const products: Product[] = [
 ];
 
 const ShopingPage = () => {
-  const [shopincCart, setShopincCart] = useState<{ [key: string]: IProductInCart }>({});
-
-  const onProductCountChange = (product: Product, number: number) => {
-    setShopincCart(prev => {
-
-      if (prev[product.id]?.count === 1 && number === -1) {
-        const { [product.id]: toDelete, ...rest } = shopincCart;
-
-        return { ...rest };
-      }
-
-      return {
-        ...prev,
-        [product.id]: { ...product, count: prev[product.id] ? prev[product.id].count + number : number }
-      }
-    });
-  };
-
   return (
     <div>
       <h1>Shoping Store</h1>
       <hr />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'wrap'
-        }}
-      >
-        {
-          products.map((product => (
-            <ProductCard
-              key={product.id}
-              product={product}
-              value={shopincCart}
-              onChange={onProductCountChange}
-            />
-          )))
-        }
 
+      <div>
         <ProductCard
           product={products[0]}
         />
-      </div>
-
-      <div className="shoping-cart">
-        {
-          Object.entries(shopincCart).map(([key, product]) => (
-            <Cart
-              key={product.id}
-              product={product}
-              value={shopincCart}
-              onChange={onProductCountChange}
-            />
-          ))
-        }
       </div>
     </div>
   );
